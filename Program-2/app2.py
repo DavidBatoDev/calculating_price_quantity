92ddc63fcd6d55f3b78fbc761de7a04189ffcde5
David Bato bato <batobatodavid20@gmail.com>
2023-12-02 18:12:19 +0800

    PROGRAM2: getting data from html

--- Program-2/app2.py
+++ Program-2/app2.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    max_apple = 0
+    remaining_money = 0
+    if request.method == "POST":
+        money = float(request.form.get("money"))
+        apple_price = float(request.form.get("apple_price"))
+        print(money, apple_price)
     return render_template("index.html")
 
 if __name__ == "__main__":
