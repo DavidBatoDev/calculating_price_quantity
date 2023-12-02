886c53cf354a05b2027e78ad502d4d27843d0b5a
David Bato bato <batobatodavid20@gmail.com>
2023-12-02 18:14:03 +0800

    PROGRAM2: function for getting the max apple avail

--- Program-2/app2.py
+++ Program-2/app2.py
@@ -2,7 +2,12 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+
+def get_max_apples(money, apple_price):
+    return int(money / apple_price)
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
     max_apple = 0
     remaining_money = 0
@@ -11,6 +16,7 @@ def index():
         apple_price = float(request.form.get("apple_price"))
         print(money, apple_price)
     return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
