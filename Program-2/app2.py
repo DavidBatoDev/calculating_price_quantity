f193f536e9a232398b9628a6374e849edcc7bbb5
David Bato bato <batobatodavid20@gmail.com>
2023-12-02 18:20:15 +0800

    PROGRAM2: handling error and formatting money

--- Program-2/app2.py
+++ Program-2/app2.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 def get_max_apples(money, apple_price):
     return int(money / apple_price)
 
+
 def calculate_remaining_money(money, apple_price):
     return money % apple_price
 
@@ -18,8 +19,17 @@ def index():
         money = float(request.form.get("money"))
         apple_price = float(request.form.get("apple_price"))
         max_apples = get_max_apples(money, apple_price)
-        remaining_money = calculate_remaining_money(money, apple_price)
-    return render_template("index.html", max_apples=max_apples, remaining_money="P" + str(remaining_money))
+        error_message = "Your money is not enough to buy an apple."
+        if max_apples == 0:
+            remaining_money = error_message
+        else:
+            remaining_money = calculate_remaining_money(money, apple_price)
+            remaining_money = (
+                format(remaining_money, ".2f") if remaining_money is not None else "0"
+            )
+    return render_template(
+        "index.html", max_apples=max_apples, remaining_money="P" + str(remaining_money)
+    )
 
 
 if __name__ == "__main__":
