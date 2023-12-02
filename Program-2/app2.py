c72b0a9ef6086503061fd76db37d3316f8f710ae
David Bato bato <batobatodavid20@gmail.com>
2023-12-02 18:18:39 +0800

    PROGRAM 2: Displaying the output to the web

--- Program-2/app2.py
+++ Program-2/app2.py
@@ -12,13 +12,14 @@ def calculate_remaining_money(money, apple_price):
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    max_apple = 0
+    max_apples = 0
     remaining_money = 0
     if request.method == "POST":
         money = float(request.form.get("money"))
         apple_price = float(request.form.get("apple_price"))
-        print(money, apple_price)
-    return render_template("index.html")
+        max_apples = get_max_apples(money, apple_price)
+        remaining_money = calculate_remaining_money(money, apple_price)
+    return render_template("index.html", max_apples=max_apples, remaining_money="P" + str(remaining_money))
 
 
 if __name__ == "__main__":
